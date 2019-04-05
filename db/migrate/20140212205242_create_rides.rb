0db58e33117c6d0df8f67899d6671f003ad412b3
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-05 02:49:49 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,7 +2,7 @@ class CreateRides < ActiveRecord::Migration[4.2]
   def change
     create_table :rides do |t|
       t.timestamps null: false
-      t.integer :passenger_id 
+      t.integer :passenger_id
       t.integer :taxi_id
     end
   end
